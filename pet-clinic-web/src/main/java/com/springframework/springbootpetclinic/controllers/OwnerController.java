adbade038335702cc284294f19c07f4ffffc84e2
devdpm <jansen.dpm@gmail.com>
2023-01-09 20:43:00 +0100

    Added Owner list and updated its controller accordingly. Closes #16

--- pet-clinic-web/src/main/java/com/springframework/springbootpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/springbootpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.springframework.springbootpetclinic.controllers;
 
+import com.springframework.springbootpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.fineAll());
+
         return "owners/index";
     }
 }
