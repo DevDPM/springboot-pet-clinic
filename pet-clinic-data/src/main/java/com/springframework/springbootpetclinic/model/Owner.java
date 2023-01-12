a7bd1ac2c7875878ea0c351d4688229c0acd04f9
devdpm <jansen.dpm@gmail.com>
2023-01-12 17:54:44 +0100

    Added info properties to Owner. closes #28

--- pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/model/Owner.java
@@ -4,9 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person{
 
+    private String address;
+    private String phoneNumber;
+    private String city;
     private Set<Pet> pets;
 
+    public String getAddress() {
+        return address;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
