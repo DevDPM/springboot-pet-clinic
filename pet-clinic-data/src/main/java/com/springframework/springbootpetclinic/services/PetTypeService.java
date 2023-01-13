291281691f5a03f3b78e9d8370a814999931fdcf
devdpm <jansen.dpm@gmail.com>
2023-01-13 06:49:13 +0100

    Added both PetTypeService & PetTypeMapService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.springbootpetclinic.services;
+
+import com.springframework.springbootpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
