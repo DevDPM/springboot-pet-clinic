ace29cc9797c4146ca92e1c80486adbbe4f3f1ba
devdpm <jansen.dpm@gmail.com>
2023-01-13 19:13:29 +0100

    Added Specialty map service. Closes #31

---
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.springframework.springbootpetclinic.services;
+
+import com.springframework.springbootpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,33 @@
+package com.springframework.springbootpetclinic.services.map;
+
+import com.springframework.springbootpetclinic.model.Specialty;
+import com.springframework.springbootpetclinic.services.SpecialtyService;
+
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
