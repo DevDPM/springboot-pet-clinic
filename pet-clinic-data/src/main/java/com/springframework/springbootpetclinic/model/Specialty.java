0cfbc1496541aac4de91710fc14bc9036241aa09
devdpm <jansen.dpm@gmail.com>
2023-01-15 18:14:34 +0100

    Vets converted to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package com.springframework.springbootpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
