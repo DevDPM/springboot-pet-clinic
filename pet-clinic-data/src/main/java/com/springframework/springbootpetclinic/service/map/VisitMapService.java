462a403972562774bde01df6fc17dc5159b29a03
devdpm <jansen.dpm@gmail.com>
2023-01-17 07:44:15 +0100

    Update VisitMapService, class corrected to use AbstractMapService.

--- pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/service/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/service/map/VisitMapService.java
@@ -1,47 +1,41 @@
 package com.springframework.springbootpetclinic.service.map;
 
 import com.springframework.springbootpetclinic.model.Visit;
-import com.springframework.springbootpetclinic.repository.VisitService;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements com.springframework.springbootpetclinic.service.VisitService {
 
-    VisitService visitService;
-
-    public VisitMapService(VisitService visitService) {
-        this.visitService = visitService;
-    }
-
     @Override
     public Set<Visit> findAll() {
-        Set<Visit> visits = new HashSet<>();
-        visitService.findAll().forEach(visit -> {
-            visits.add(visit);
-        });
-        return visits;
+        return super.findAll();
     }
 
     @Override
     public Visit findById(Long id) {
-        return visitService.findById(id).isPresent() ? visitService.findById(id).get() : null;
+        return super.findById(id);
     }
 
     @Override
     public Visit save(Visit visit) {
-        return visitService.save(visit);
+        if (    (visit.getPet() == null) ||
+                (visit.getPet().getOwner() == null) ||
+                (visit.getPet().getId() == null) ||
+                (visit.getPet().getOwner().getId() == null))
+            throw new RuntimeException("Invalide visit");
+
+        return super.save(visit);
     }
 
     @Override
     public void delete(Visit visit) {
-        visitService.delete(visit);
+        super.delete(visit);
     }
 
     @Override
     public void deleteById(Long id) {
-        visitService.deleteById(id);
+        super.deleteById(id);
     }
 }
